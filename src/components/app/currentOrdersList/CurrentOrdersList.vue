c3721d9e3a99441b1a85ea4b8faa0bbacc3716d3
Ruslan Musaev <rusya.lay@gmail.com>
2023-01-21 22:42:49 +0400

    wip: orders list "

--- src/components/app/currentOrdersList/CurrentOrdersList.vue
+++ src/components/app/currentOrdersList/CurrentOrdersList.vue
@@ -34,6 +34,53 @@
         </template>
       </i18n-t>
     </template>
+    <template #cell(pair)="{ data: currency }">
+      <div :class="$style.pairCell">
+        <CurrencyLogo :currency="currency" />
+        <span :class="$style.pairName">
+          {{ currency }}
+        </span>
+      </div>
+    </template>
+    <template #cell(type)="{ data: orderDirection }">
+      <span :class="[$style.orderDirection, $style[orderDirection]]">
+        {{ t(`order.direction.${ ({ sell: 'short', buy: 'long' }[orderDirection]) }`) }}
+      </span>
+    </template>
+    <template #cell(volume)="{ data }">
+      {{ data }}
+    </template>
+    <template #cell(coins)="{ data }">
+      {{ data }}
+    </template>
+    <template #cell(prices)="{ data }">
+      <div :class="$style.pricesCell">
+        <span>
+          {{ data.orderPrice }}
+        </span>
+        <span :class="$style.pricesCellCurrent">
+          {{ data.current }}
+        </span>
+      </div>
+    </template>
+    <template #cell(sl)="{ data }">
+      {{ data }}
+    </template>
+    <template #cell(pnl)="{ data }">
+      {{ data }}
+    </template>
+    <template #cell(tp)="{ data }">
+      {{ data }}
+    </template>
+    <template #cell(date)="{ data }">
+      {{ data }}
+    </template>
+    <template #cell(comment)>
+      comment
+    </template>
+    <template #cell(options)>
+      options
+    </template>
   </Table>
 </template>
 
@@ -42,6 +89,8 @@ import { computed } from 'vue';
 import { useI18n } from 'vue-i18n';
 import Table from '@/components/core/table/Table.vue';
 import InlineSpace from '@/components/core/inlineSpace/InlineSpace.vue';
+import CurrencyLogo from '@/components/core/currencyLogo/CurrencyLogo.vue';
+import { currency } from '@/api/types/currency';
 import { CurrentOrdersTableColumn, CurrentOrdersTableRecord } from './index';
 
 const { t } = useI18n();
@@ -108,10 +157,32 @@ const columns = computed<CurrentOrdersTableColumn[]>(() => [
   },
 ]);
 
-const records = computed<CurrentOrdersTableRecord[]>(() => []);
+const records = computed<CurrentOrdersTableRecord[]>(() => [
+  {
+    id: 1,
+    data: {
+      pair: currency.BTC,
+      type: 'sell',
+      volume: 0,
+      coins: 0,
+      prices: {
+        orderPrice: 8000,
+        current: 7000,
+      },
+      sl: 0,
+      pnl: 0,
+      tp: 0,
+      date: 0,
+      comment: {},
+      options: {},
+    },
+  },
+]);
 </script>
 
 <style lang="scss" module>
+@import "src/assets/styles/utils";
+
 .ordersList {}
 
 .quoteCurrency {
@@ -125,5 +196,37 @@ const records = computed<CurrentOrdersTableRecord[]>(() => []);
 .pnlColumnValue {
   color: rgb(var(--color-success));
   font-weight: 600;
+}
+
+.pairCell {
+  display: flex;
+  align-items: center;
+}
+
+.pairName {
+  color: rgb(var(--color-accent-2));
+  @include title2;
+  font-weight: 600;
+  margin-left: 10px;
+}
+
+.orderDirection {
+  font-weight: 600;
+  @include title3;
+  &.sell {
+    color: rgb(var(--color-danger));
+  }
+  &.buy {
+    color: var(--color-success);
+  }
+}
+
+.pricesCell {
+  display: flex;
+}
+
+.pricesCellCurrent {
+  margin-left: 5px;
+  color: rgb(var(--color-accent-2));
 }
 </style>

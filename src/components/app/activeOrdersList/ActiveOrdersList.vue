35ad7da884f75e120ff79f4d6c07b197f93235e0
Ruslan Musaev <rusya.lay@gmail.com>
2023-01-22 19:05:44 +0400

    wip: active orders list

--- src/components/app/activeOrdersList/ActiveOrdersList.vue
+++ src/components/app/activeOrdersList/ActiveOrdersList.vue
@@ -85,99 +85,22 @@
 </template>
 
 <script async setup lang="ts">
-import { computed } from 'vue';
 import { useI18n } from 'vue-i18n';
 import Table from '@/components/core/table/Table.vue';
 import InlineSpace from '@/components/core/inlineSpace/InlineSpace.vue';
 import CurrencyLogo from '@/components/core/currencyLogo/CurrencyLogo.vue';
-import { currency } from '@/api/types/currency';
-import { CurrentOrdersTableColumn, CurrentOrdersTableRecord } from './index';
+import { useActiveOrdersList } from '@/hooks/useActiveOrdersList';
 
 const { t } = useI18n();
 
-const columns = computed<CurrentOrdersTableColumn[]>(() => [
-  {
-    label: t('ordersList.column.pair'),
-    slug: 'pair',
-    size: 1,
-  },
-  {
-    label: t('ordersList.column.type'),
-    slug: 'type',
-    size: 0.7,
-  },
-  {
-    label: '',
-    slug: 'volume',
-    size: 1.1,
-  },
-  {
-    label: t('ordersList.column.coins'),
-    slug: 'coins',
-    size: 1,
-  },
-  {
-    label: '',
-    slug: 'prices',
-    size: 1.7,
-  },
-  {
-    label: t('ordersList.column.sl'),
-    slug: 'sl',
-    size: 0.5,
-    align: 'center',
-  },
-  {
-    label: '',
-    slug: 'pnl',
-    size: 1.5,
-    align: 'center',
-  },
-  {
-    label: t('ordersList.column.tp'),
-    slug: 'tp',
-    size: 0.5,
-    align: 'center',
-  },
-  {
-    label: t('ordersList.column.date'),
-    slug: 'date',
-    size: 1,
-  },
-  {
-    label: t('ordersList.column.comment'),
-    slug: 'comment',
-    size: 0.5,
-    align: 'center',
-  },
-  {
-    label: '',
-    slug: 'options',
-    size: 0.7,
-  },
-]);
+const {
+  columns,
+  records,
+  isLoading,
+  getList,
+} = useActiveOrdersList();
 
-const records = computed<CurrentOrdersTableRecord[]>(() => [
-  {
-    id: 1,
-    data: {
-      pair: currency.BTC,
-      type: 'sell',
-      volume: 0,
-      coins: 0,
-      prices: {
-        orderPrice: 8000,
-        current: 7000,
-      },
-      sl: 0,
-      pnl: 0,
-      tp: 0,
-      date: 0,
-      comment: {},
-      options: {},
-    },
-  },
-]);
+await getList();
 </script>
 
 <style lang="scss" module>
---
+++ src/hooks/useActiveOrdersList.ts
@@ -0,0 +1,113 @@
+import { computed, ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { CurrentOrdersTableColumn, CurrentOrdersTableRecord } from '@/components/app/activeOrdersList';
+import { useMarketStore } from '@/stores/market';
+import { Order } from '@/api/types/order';
+
+export const useActiveOrdersList = () => {
+  const { t } = useI18n();
+  const marketStore = useMarketStore();
+
+  const columns = computed<CurrentOrdersTableColumn[]>(() => [
+    {
+      label: t('ordersList.column.pair'),
+      slug: 'pair',
+      size: 1,
+    },
+    {
+      label: t('ordersList.column.type'),
+      slug: 'type',
+      size: 0.7,
+    },
+    {
+      label: '',
+      slug: 'volume',
+      size: 1.1,
+    },
+    {
+      label: t('ordersList.column.coins'),
+      slug: 'coins',
+      size: 1,
+    },
+    {
+      label: '',
+      slug: 'prices',
+      size: 1.7,
+    },
+    {
+      label: t('ordersList.column.sl'),
+      slug: 'sl',
+      size: 0.5,
+      align: 'center',
+    },
+    {
+      label: '',
+      slug: 'pnl',
+      size: 1.5,
+      align: 'center',
+    },
+    {
+      label: t('ordersList.column.tp'),
+      slug: 'tp',
+      size: 0.5,
+      align: 'center',
+    },
+    {
+      label: t('ordersList.column.date'),
+      slug: 'date',
+      size: 1,
+    },
+    {
+      label: t('ordersList.column.comment'),
+      slug: 'comment',
+      size: 0.5,
+      align: 'center',
+    },
+    {
+      label: '',
+      slug: 'options',
+      size: 0.7,
+    },
+  ]);
+
+  const records = ref<CurrentOrdersTableRecord[]>([]);
+
+  const isLoading = ref(false);
+  const getList = async () => {
+    isLoading.value = true;
+    const { result, data } = await marketStore.getOrderList();
+    isLoading.value = false;
+
+    if (!result) return;
+
+    const limitOrders = data.filter((order: Order) => order.order_type === 'limit');
+
+    records.value = limitOrders.map((order: Order) => ({
+      id: order.id,
+      data: {
+        pair: marketStore.pairsMap?.[order.pair]?.base,
+        type: order.side,
+        volume: 0, /* FIXME */
+        coins: order.quantity,
+        prices: {
+          orderPrice: order.price,
+          current: 0, /* FIXME */
+        },
+        sl: 0, /* FIXME */
+        pnl: 0, /* FIXME */
+        tp: 0, /* FIXME */
+        date: '',
+        comment: order,
+        options: order,
+      },
+    }));
+    console.log(records.value);
+  };
+
+  return {
+    columns,
+    records,
+    isLoading,
+    getList,
+  };
+};

e8374a459bdcfcf6c5e43a227f699f1df37aff6e
Ruslan Musaev <rusya.lay@gmail.com>
2023-04-03 17:26:55 +0400

    wip: stats

--- src/components/core/barChart/createBarChart.ts
+++ src/components/core/barChart/createBarChart.ts
@@ -5,6 +5,7 @@ import {
   scaleBand,
   scaleLinear,
   select,
+  Selection,
 } from 'd3';
 import { toAbsolute } from '@/utils/number';
 import { multiply } from '@/helpers/number';
@@ -20,66 +21,41 @@ export interface CreateBarChartProps {
   topMargin?: number;
 }
 
-export const createBarChart = ({
-  container,
-  data,
-  labelGap = 10,
-  minWidthPerBar = 50,
-  barBorderRadius = 5,
-  topMargin = 30,
-}: CreateBarChartProps) => {
-  // Define the dimensions of the chart
-  const numBars = data.length;
-  const width = Math.max(700, numBars * minWidthPerBar);
-  const height = 300;
+type SVGContainer = Selection<SVGElement, null, null, null>;
 
-  // Create the x scale
-  const xScale = scaleBand<number>()
-    .domain(range(data.length))
-    .range([0, width])
-    .padding(0.1)
-    .paddingInner(0.2);
-
+const createXAxis = (
+  svg: SVGContainer,
+  data: BarChartData,
+  xScale: any,
+  height: number,
+  topMargin: number,
+) => {
   const xAxisLabels = data.map(([label]) => label);
+  const xAxis = axisBottom(xScale).tickFormat((d, i) => xAxisLabels[i]);
 
-  const xAxis = axisBottom(xScale)
-    .tickFormat((d, i) => xAxisLabels[i]);
+  const xAxisElement = svg
+    .append('g')
+    .attr('transform', `translate(0, ${height - topMargin})`)
+    .attr('style', 'color: white')
+    .call(xAxis);
 
-  // Create the y scale
-  const yScale = scaleLinear<number>()
-    .domain([0, max(data.map(([, value]) => toAbsolute(value))) as number])
-    .range([height - topMargin, topMargin]);
+  // remove line on xAxis
+  xAxisElement.select('.domain').attr('stroke', 'none');
 
-  // Create the SVG element
-  const svg = select(container)
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height);
+  // remove vertical dashes on xAxis
+  xAxisElement.selectAll('line').attr('stroke-width', 0);
 
-  // Add value labels to the bars
-  const barValueLabels = svg.selectAll<SVGTextElement, number>('text')
-    .data(data)
-    .enter()
-    .append('text')
-    .text(([, value]) => value)
-    .attr('x', (d, i) => xScale(i) as number + xScale.bandwidth() / 2)
-    .attr('y', ([, value]) => yScale(toAbsolute(value)) - labelGap)
-    .style('font-size', '12px')
-    .style('text-anchor', 'middle')
-    .style('fill', 'white');
+  // set styles to xAxis labels
+  xAxisElement.selectAll('text').style('font-size', '12px');
+};
 
-  // Labels appearance animation
-  barValueLabels
-    .attr('y', ([, value]) => yScale(toAbsolute(value)) - labelGap - 300)
-    .attr('opacity', 0)
-    .transition()
-    .duration(300)
-    .delay((d, i) => multiply(data.length - i, 40))
-    .attr('y', ([, value]) => yScale(toAbsolute(value)) - labelGap)
-    .attr('opacity', 1)
-    .ease();
-
-  // Create the bars
+const createBars = (
+  svg: SVGContainer,
+  data: BarChartData,
+  xScale: any,
+  yScale: any,
+  barBorderRadius: number,
+) => {
   const bars = svg.selectAll<SVGRectElement, number>('rect')
     .data(data)
     .enter()
@@ -92,28 +68,48 @@ export const createBarChart = ({
     .attr('rx', barBorderRadius)
     .attr('ry', barBorderRadius);
 
-  // Append xAxis to svg container
-  const xAxisElement = svg.append('g')
-    .attr('transform', `translate(0, ${height - topMargin})`)
-    .attr('style', 'color: white')
-    .call(xAxis);
+  return bars;
+};
 
-  // remove line on xAxis
-  xAxisElement
-    .select('.domain')
-    .attr('stroke', 'none');
+const createBarValueLabels = (
+  svg: SVGContainer,
+  data: BarChartData,
+  xScale: any,
+  yScale: any,
+  labelGap: number,
+) => {
+  const barValueLabels = svg.selectAll<SVGTextElement, number>('text')
+    .data(data)
+    .enter()
+    .append('text')
+    .text(([, value]) => value)
+    .attr('x', (d, i) => xScale(i) as number + xScale.bandwidth() / 2)
+    .attr('y', ([, value]) => yScale(toAbsolute(value)) - labelGap)
+    .style('font-size', '12px')
+    .style('text-anchor', 'middle')
+    .style('fill', 'white');
 
-  // remove vertical dashes on xAxis
-  xAxisElement
-    .selectAll('line')
-    .attr('stroke-width', 0);
+  return barValueLabels;
+};
 
-  // set styles to xAxis labels
-  xAxisElement
-    .selectAll('text')
-    .style('font-size', '12px');
+const animateBarValueLabels = (
+  barValueLabels: any,
+  data: BarChartData,
+  yScale: any,
+  labelGap: number,
+) => {
+  barValueLabels
+    .attr('y', ([, value]) => yScale(toAbsolute(value)) - labelGap - 300)
+    .attr('opacity', 0)
+    .transition()
+    .duration(300)
+    .delay((d, i) => multiply(data.length - i, 40))
+    .attr('y', ([, value]) => yScale(toAbsolute(value)) - labelGap)
+    .attr('opacity', 1)
+    .ease();
+};
 
-  // Bars appearance animation
+const animateBars = (bars: any, data: BarChartData, yScale: any) => {
   bars
     .attr('y', ([, value]) => yScale(toAbsolute(value)) - 60)
     .attr('opacity', 0)
@@ -123,4 +119,42 @@ export const createBarChart = ({
     .attr('opacity', 1)
     .delay((d, i) => multiply(data.length - i, 40))
     .ease();
+};
+
+export const createBarChart = ({
+  container,
+  data,
+  labelGap = 10,
+  minWidthPerBar = 50,
+  barBorderRadius = 5,
+  topMargin = 30,
+}: CreateBarChartProps) => {
+  const numBars = data.length;
+  const width = Math.max(700, numBars * minWidthPerBar);
+  const height = 300;
+
+  const xScale = scaleBand<number>()
+    .domain(range(data.length))
+    .range([0, width])
+    .padding(0.1)
+    .paddingInner(0.2);
+
+  const yScale = scaleLinear<number>()
+    .domain([0, max(data.map(([, value]) => toAbsolute(value))) as number])
+    .range([height - topMargin, topMargin]);
+
+  const svg = select(container)
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+  const bars = createBars(svg, data, xScale, yScale, barBorderRadius);
+
+  animateBars(bars, data, yScale);
+
+  const barValueLabels = createBarValueLabels(svg, data, xScale, yScale, labelGap);
+
+  createXAxis(svg, data, xScale, height, topMargin);
+
+  animateBarValueLabels(barValueLabels, data, yScale, labelGap);
 };

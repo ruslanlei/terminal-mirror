00ee4321744483f1d967ed90ba2d090b5b84cafb
Ruslan Musaev <rusya.lay@gmail.com>
2023-04-18 13:01:17 +0400

    -

--- src/pages/auth/account-confirm-email/[token].vue
+++ src/pages/auth/account-confirm-email/[token].vue
@@ -1,43 +1,13 @@
 <template>
-  <div :class="$style.verifyEmail">
-    <Loader v-if="isLoading" />
-    <div
-      v-else-if="errorText"
-      :class="$style.error"
-    >
-      {{ errorText }}
-    </div>
-  </div>
+  <ConfirmEmail />
 </template>
 
 <route lang="yaml">
-  meta:
-    guestRequired: true
-    layout: auth
+meta:
+  guestRequired: true
+  layout: auth
 </route>
 
 <script setup lang="ts">
-import { useEmailVerification } from '@/hooks/useEmailVerification';
-import Loader from '@/components/core/loader/Loader.vue';
-
-const {
-  errorText,
-  isLoading,
-  verify,
-} = useEmailVerification();
-
-verify();
+import ConfirmEmail from '@/components/app/confirmEmail/ConfirmEmail.vue';
 </script>
-
-<style lang="scss" module>
-@import "../../../assets/styles/utils";
-
-.verifyEmail {
-  padding: 20px 0;
-}
-
-.error {
-  @include title3;
-  color: rgb(var(--color-danger));
-}
-</style>
